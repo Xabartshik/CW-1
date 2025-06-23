5a3ca28065b444ad589432072b944175536c84f3
Xabartshik <danoshlakov12@gmail.com>
2025-06-23 15:03:17 +1000

    Убрал ненужные файлы

--- TransportManagementSystem/Program.cs
+++ TransportManagementSystem/Program.cs
@@ -6,30 +6,7 @@ internal class Program
 
     static void Main(string[] args)
     {
-        var myCar = new Car
-        {
-            Brand = "Toyota",
-            Model = "Corolla",
-            Year = 2022,
-            MaxSpeed = 195,
-            Doors = 4
-        };
 
-        myCar.Start();          
-        myCar.OpenTrunk();      
-        Console.WriteLine(myCar.GetInfo());
-        ServiceVehicle(myCar);
-
-        var tesla = new ElectricCar
-        {
-            Brand = "Tesla",
-            Model = "Y3",
-            Year = 2022,
-            MaxSpeed = 200
-        };
-
-        tesla.Start();
-        Console.WriteLine(tesla.GetInfo());
     }
 
 
