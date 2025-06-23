ea8f6185bd4d7224d4b5624c67dd789ec649bd1c
Xabartshik <danoshlakov12@gmail.com>
2025-06-23 12:35:41 +1000

    Добавлен класс Vehicle. Добавлена демонстрация работы класса Vehicle

--- TransportManagementSystem/Program.cs
+++ TransportManagementSystem/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace TransportManagementSystem
+﻿using TransportManagementSystem;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Goodbye, World!");
-        }
+        Vehicle car = new Vehicle();
+        car.brand = "Toyota";
+        car.model = "Camry";
+        car.year = 2023;
+        car.maxSpeed = 220;
+
+        Vehicle truck = new Vehicle();
+        truck.brand = "Volvo";
+        truck.model = "FH16";
+        truck.year = 2022;
+        truck.maxSpeed = 120;
+
+        Console.WriteLine(car.GetInfo());
+        car.Start();
+        car.Stop();
+
+        Console.WriteLine(truck.GetInfo());
+        truck.Start();
+        truck.Stop();
+
+        Console.ReadLine();
+
     }
 }
---
+++ TransportManagementSystem/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportManagementSystem
+{
+    public class Vehicle
+    {
+        public string brand;
+        public string model;
+        public int year;
+        public double maxSpeed;
+
+        // Метод для запуска двигателя
+        public void Start()
+        {
+            Console.WriteLine($"{brand} {model} завёл двигатель!");
+        }
+
+        // Метод для остановки
+        public void Stop()
+        {
+            Console.WriteLine($"{brand} {model} остановился.");
+        }
+
+        // Метод для получения информации
+        public string GetInfo()
+        {
+            return $"{brand} {model} ({year} год)";
+        }
+
+    }
+
+
+}
