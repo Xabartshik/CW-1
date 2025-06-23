92900c6d2087e13b89e2438681dbf1c0ffcf472f
Xabartshik <danoshlakov12@gmail.com>
2025-06-23 13:00:00 +1000

    Добавлен класс Car : Vehicle

---
+++ TransportManagementSystem/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportManagementSystem
+{
+    public class Car : Vehicle  
+    {
+        public int Doors { get; set; } 
+        public void OpenTrunk() => Console.WriteLine($"{Brand} открыл багажник");
+    }
+}
