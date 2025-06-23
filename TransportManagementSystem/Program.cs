9441c1c0c4f2afa803653a63354e99815fe6f011
Xabartshik <danoshlakov12@gmail.com>
2025-06-23 15:06:45 +1000

    Демонстрация работы

--- TransportManagementSystem/Program.cs
+++ TransportManagementSystem/Program.cs
@@ -2,10 +2,25 @@
 
 internal class Program
 {
-    static void ServiceVehicle(Vehicle v) => Console.WriteLine($"Обслуживаем {v.GetInfo()}");
 
     static void Main(string[] args)
     {
+        Vehicle car = new Vehicle
+        {
+            Brand = "Toyota",
+            Model = "Camry",
+            Year = 2023,
+            MaxSpeed = 220
+        };
+        car.CanReachSpeed(200);
+        Vehicle truck = new Vehicle
+        {
+            Brand = "Volvo",
+            Model = "FH16",
+            Year = 2022,
+            MaxSpeed = 120
+        };
+        truck.CanReachSpeed(200);
 
     }
 
