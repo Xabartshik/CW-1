fd448d0c592afbe9d70ca758974b5b8674b1189e
Xabartshik <danoshlakov12@gmail.com>
2025-06-23 15:38:52 +1000

    Класс Time заменен на класс Cursor

---
+++ TransportManagementSystem/Cursor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportManagementSystem
+{
+    //Класс курсора, передвигаемого по экрану
+    internal class Cursor : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine($"Курсор двигается по экрану");
+        }
+    }
+}
---
+++ TransportManagementSystem/IRepairable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportManagementSystem
+{
+    public interface IRepairable
+    {
+        string Model { get; set; }
+        string Brand { get; set; }
+    }
+}
