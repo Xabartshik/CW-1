6a50018b6d10e05c4dde4a1847cbe6d03636e1c6
Xabartshik <danoshlakov12@gmail.com>
2025-06-23 15:51:27 +1000

    Добавлены классы: Engine, ServiceBox Интерфейс: IRepairable Демонстрация работы с ними

--- TransportManagementSystem/Program.cs
+++ TransportManagementSystem/Program.cs
@@ -7,10 +7,30 @@ internal class Program
 
     static void Main(string[] args)
     {
+        var myCar = new Car("Toyota", "Corolla")
+        {
+            Year = 2022,
+            MaxSpeed = 195,
+            Doors = 4
+        };
+
         var sportcar = new SportCar("Toyota", "Камри 3.5")
         {
             SpoilerType = "Крыло"
         };
+        // Program.cs
+        var carServiceBox = new ServiceBox<Car>("Бокс ремонта авто");
+        carServiceBox.ShofInfo();
+        carServiceBox.Repair(myCar);
+        carServiceBox.ShofInfo();
+
+        Console.WriteLine();
+
+        var engineServiceBox = new ServiceBox<Engine>("Бокс ремонта двигателей");
+        engineServiceBox.ShofInfo();
+        engineServiceBox.Repair(new Engine { Brand = "Subaru", Model = "EJ257" }); // создаем двигатель, заполняем его через инициализатор и сразу передаем в бокс
+        engineServiceBox.ShofInfo();
+
 
     }
 
