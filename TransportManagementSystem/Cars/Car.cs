dcec989e36348590cb23f60cbff9f7322871e5fc
Xabartshik <danoshlakov12@gmail.com>
2025-06-23 16:12:38 +1000

    Переместил классы машин в папку Cars

--- TransportManagementSystem/Cars/Car.cs
+++ TransportManagementSystem/Cars/Car.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TransportManagementSystem.Contracts;
 
-namespace TransportManagementSystem
+namespace TransportManagementSystem.Cars
 {
     public class Car : Vehicle, IMovable, IRepairable
     {
--- TransportManagementSystem/Program.cs
+++ TransportManagementSystem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using TransportManagementSystem;
+using TransportManagementSystem.Cars;
 
 internal class Program
 {
